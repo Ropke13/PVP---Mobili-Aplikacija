4ff8a9293d16d515a29ce33b70f5226ea18b05cb
Dominykas Petkevičius <dominykas.petkevicius@gmail.com>
2022-04-15 14:43:46 +0300

    Wrong screen with flutter_tex

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -12,7 +12,7 @@ List<String> generateNewQuestion() {
 
   String explain = "Sudauginus $digit1 su $digit2 gauname $answer";
 
-  return ["Kiek yra " + digit1.toString() + "*" + digit2.toString() + "?", answer, explain];
+  return ["Kiek yra \\(" + digit1.toString() + "\\cdot" + digit2.toString() + "\\)?", answer, explain];
 }
 
 // returns [question, answers, correctAnswer, explanation]
@@ -22,19 +22,21 @@ List getNumberQuestion() {
   switch (index) {
     case 0:
       return [
-        "Duotos trys aibės:  Kuris iš toliau pateiktų teiginių yra klaidingas?",
+        "Duotos trys aibės: \\(A=\\{2,3,5,9\\}, B = \\{5,6,9\\}, C=\\{3,5,6,7,8\\}\\)  Kuris iš toliau pateiktų teiginių yra klaidingas?",
         [
-          "A",
-          "B",
-          "C",
-          "D",
+          "\\((B \\setminus C) \\subset A\\)",
+          "\\(A \\cap  B \\cap  C = \\{5\\}\\)",
+          "\\(B \\subset A \\)",
+          "\\(B \\cup C = \\{3, 5, 6, 7, 8, 9\\}\\)",
         ],
         2,
-        """C, nes visi pirmoje aibėje esantys elementai turi būti antroje aibėje: . Kadangi šešeto nėra antroje aibėje, tai teiginys yra klaidingas. 
+        """C, nes visi pirmoje aibėje esantys elementai turi būti antroje aibėje: \\(\\{5, 6, 9\\} \\subset \\{ 2,3,5,9\\}\\).
+Kadangi šešeto nėra antroje aibėje, tai teiginys yra klaidingas. 
 A, B, D teiginiai nėra klaidingi, nes 
-A:  devyni priklauso aibei ;
-B apjungiame bendrus aibės elementus ir gauname: ;
-D sujungiame visus aibių elementus į vieną aibę: """
+A: \\(\\{5,6,9\\} \\setminus \\{ 3,5,6,7,8\\} = \\{9\\}\\) devyni priklauso aibei \\(A=\\{2,3,5,9\\}\\);
+B apjungiame bendrus aibės elementus ir gauname: 
+\\(A \\cap B = \\{2,3,5,9\\} \\cap \\{5,6,9\\} = \\{5,9\\}, \\{5,9\\} \\cap C = \\{5,9\\} \\cap \\{3,5,6,7,8\\} = \\{5\\}\\);
+D sujungiame visus aibių elementus į vieną aibę: \\(B \\cup C = \\{5,6,9\\} \\cup \\{3,5,6,7,8\\} = \\{3, 5, 6, 7, 8, 9\\}\\)"""
       ];
     default:
       throw ("No question");
--- lib/wrong.dart
+++ lib/wrong.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:matematika/list.dart';
+import 'package:flutter_tex/flutter_tex.dart';
 
 class Wrong extends StatelessWidget {
   final String theme;
@@ -27,41 +27,64 @@ class Wrong extends StatelessWidget {
         body: SafeArea(
           child: Column(children: <Widget>[
             const Divider(color: Color.fromARGB(255, 21, 21, 21), thickness: 2),
-            Expanded(
-              flex: 3,
-              child: Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(question, textAlign: TextAlign.center, style: const TextStyle(color: Colors.white, fontSize: 20.0)),
-                      Text(wrongAnswer, style: const TextStyle(color: Colors.red, fontSize: 20.0, decoration: TextDecoration.lineThrough)),
-                      Text(answer, style: const TextStyle(color: Colors.green, fontSize: 20.0)),
-                    ],
-                  )
+            TeXView(
+                child: TeXViewColumn(
+                  children: <TeXViewWidget>[
+                    TeXViewContainer(
+                        child: TeXViewColumn(children: <TeXViewWidget>[
+                          TeXViewDocument(question,
+                              style: TeXViewStyle(
+                                contentColor: Colors.white,
+                                fontStyle: TeXViewFontStyle(fontSize: 20),
+                                textAlign: TeXViewTextAlign.center,
+                                padding: const TeXViewPadding.only(
+                                  sizeUnit: TeXViewSizeUnit.pt,
+                                  top: 10,
+                                  bottom: 10,
+                                ),
+                              )),
+                          TeXViewDocument(wrongAnswer,
+                              style: TeXViewStyle(
+                                contentColor: Colors.red,
+                                fontStyle: TeXViewFontStyle(fontSize: 20),
+                                textAlign: TeXViewTextAlign.center,
+                              )),
+                          TeXViewDocument(answer,
+                              style: TeXViewStyle(
+                                contentColor: Colors.green,
+                                fontStyle: TeXViewFontStyle(fontSize: 20),
+                                textAlign: TeXViewTextAlign.center,
+                              )),
+                        ]),
+                        style: const TeXViewStyle(
+                          padding: TeXViewPadding.only(
+                            sizeUnit: TeXViewSizeUnit.pt,
+                            left: 30,
+                            right: 30,
+                          ),
+                        )),
+                    TeXViewDocument("Neteisingai",
+                        style: TeXViewStyle(
+                          contentColor: Colors.red,
+                          fontStyle: TeXViewFontStyle(fontSize: 50, fontWeight: TeXViewFontWeight.bold),
+                          textAlign: TeXViewTextAlign.center,
+                        )),
+                    TeXViewDocument(explain,
+                        style: TeXViewStyle(
+                          contentColor: Colors.white,
+                          fontStyle: TeXViewFontStyle(fontSize: 15),
+                          textAlign: TeXViewTextAlign.center,
+                          padding: const TeXViewPadding.only(
+                            sizeUnit: TeXViewSizeUnit.pt,
+                            left: 30,
+                            right: 30,
+                            top: 10,
+                            bottom: 10,
+                          ),
+                        )),
+                  ],
                 ),
-              ),
-              const Expanded(
-                flex: 1,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                      "Neteisingai",
-                      style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold, fontSize: 50.0),
-                  )
-                )
-              ),
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-                  child: Text(
-                    explain,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(color: Colors.white, fontSize: 15.0, height: 1),
-                )
-              )
-            ),
+                renderingEngine: const TeXViewRenderingEngine.katex()),
             Expanded(
               flex: 1,
               child: InkWell(
